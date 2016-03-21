a6c0ef216087a8b38b92d5f5a91602b6eb1898a2
ElijahButers <Elijahbuters@gmail.com>
2016-03-21 19:44:37 +0200

    Added a button that lets users add people to the app: navigationItem.rightBarButtonItem.

--- NamesToFaces.Project10/ViewController.swift
+++ NamesToFaces.Project10/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "addNewPerson")
     }
 
     override func didReceiveMemoryWarning() {

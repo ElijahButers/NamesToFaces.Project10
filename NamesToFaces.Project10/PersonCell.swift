e21e4cd03c1724fdb053cd86718d3f7c00a15d8a
ElijahButers <Elijahbuters@gmail.com>
2016-03-21 16:58:32 +0200

    Added a PersonCell class that inherits from UICollectionViewCell.

---
+++ NamesToFaces.Project10/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  NamesToFaces.Project10
+//
+//  Created by User on 3/21/16.
+//  Copyright © 2016 Elijah Buters. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+}

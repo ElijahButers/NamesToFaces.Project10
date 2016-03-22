bd2c3a63ed66acf9c89adba63e5ffbb5b3cef170
ElijahButers <Elijahbuters@gmail.com>
2016-03-22 12:11:18 +0200

    Added a custom class Person that inherits from NSObject.

---
+++ NamesToFaces.Project10/Person.swift
@@ -0,0 +1,13 @@
+//
+//  Person.swift
+//  NamesToFaces.Project10
+//
+//  Created by User on 3/22/16.
+//  Copyright © 2016 Elijah Buters. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+
+}
